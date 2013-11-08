02a78f069982e506e47fa77385f201691501b142
Isaac Hunter <isaac.hunter.c@gmail.com>
2013-11-07 22:27:32 -0500

    Made Personal Website

---
+++ include/bespoke/isaac.hunter.js
@@ -0,0 +1,83 @@
+
+function slidesToGrid()
+{
+	$('#portfolio article').removeClass("animate");
+	$('#portfolio section').removeAttr("style");
+	$('#portfolio section.bespoke-active').css({
+				'z-index':'50',
+				"left" : "10%",
+				"bottom" : "10%",
+				"top" : "10%",
+				"right" : "10%",
+				'width' : '80%',
+				'height' : '80%'
+	});
+	setTimeout(function() {
+		viewGrid();
+	}, 150);
+}
+function viewGrid()
+{
+	$('#portfolio').addClass('grid');
+	$('#portfolio').removeClass('slides');
+	i = 0;
+	j = 0;
+	k = 0;
+	$('#portfolio.grid section').each(function () {
+		$(this).removeClass();
+		$(this).css({
+			"left" : i + '%',
+			"bottom" : (55 - j) + '%',
+			"top" : j + '%',
+			"right" : (70 - i) + '%',
+			"width" : '30%',
+			"height" : '45%'
+		});
+
+		$(this).attr('id', k);
+		i += 35;
+		k ++;
+		if (i > 100)
+		{
+			i = 0;
+			j += 55;
+		}
+
+	});
+
+	$('#portfolio.grid section').click(function() {
+		$('#portfolio section').css({"z-index" : "0"});
+		that = this;
+		$(that).addClass('clicked');
+
+		setTimeout(function() {
+			$(that).css({
+				'z-index':'50',
+				"left" : "12%",
+				"bottom" : "5%",
+				"top" : "5%",
+				"right" : "12%",
+				'width' : '76%',
+				'height' : '90%'
+			});
+		}, 100);
+			
+		setTimeout(function() {
+			viewSlides($(that).attr('id'));
+		}, 1000);
+	});
+}
+function viewSlides(index)
+{
+	$('#portfolio section').removeAttr("style");
+	$('#portfolio').addClass('slides')
+	$('#portfolio').removeClass('grid');
+	var one = bespoke.horizontal.from('#portfolio.slides > article');
+	one.slide(index);
+	setTimeout(function() {
+		$('#portfolio article').addClass("animate");
+	}, 800);
+	$('#portfolio > .close').click( function () {
+		slidesToGrid();
+	});
+} 
